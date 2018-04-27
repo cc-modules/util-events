b9758c3260212d5c64214d629ca05736bfe62078
wangweihua <aztack@163.com>
2018-04-27 11:13:45 +0800

    bigbang

---
+++ util-events.js
@@ -0,0 +1,33 @@
+module.exports = {
+  /**
+   * Helper function to emit events
+   * @param {String|cc.Component.EventHanlder} events 
+   */
+  $emitEvents(events) {
+    var e = events;
+    if (typeof events === 'string') {
+      e = this[events];
+    }
+    if (typeof e !== 'undefined') {
+      cc.Component.EventHandler.emitEvents(e);
+    }
+  },
+  /**
+   * Call a event handler and return the result
+   * @param {String|cc.Component.EventHandler} events 
+   */
+  $callHandler(events, args = []) {
+    var e = events;
+    if (typeof events === 'string') {
+      e = this[events];
+    }
+    var result;
+    if (typeof e !== 'undefined') {
+      const target = e.target;
+      const comp = target.getComponent(e.component);
+      const handler = comp[e.handler];
+      result = handler.apply(comp, args);
+    }
+    return result;
+  }
+}
